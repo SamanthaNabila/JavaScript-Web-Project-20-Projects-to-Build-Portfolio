779e0fe7d49af2c1c86c7829c8ef163fc24557c0
Jannatul Ferdous Nabila <samanthanabila47@gmail.com>
2026-04-24 15:41:36 +0600

    Infinity Scroll Project

---
+++ Infinity Scroll/script.js
@@ -0,0 +1,83 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+let photosArray = [];
+
+//Unsplash Api
+const count = 5;
+const apiKey = "-Fr2wc5G6xTWcFGkVf9Md3e0JyhGjvwqGWeqzgIHucc";
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//check loader
+function imageLoaded() {
+  imagesLoaded++;
+  console.log("imagesLoaded");
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    const count = 30;
+  }
+}
+
+//helper Function
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+//creat elements for links a& photos , add to dom
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
+  //Run function for each object in Photos Array
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    //creat <img> for photo
+    const img = document.createElement("img");
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    img.addEventListener("load", imageLoaded);
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+//Get Photos from API
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch {
+    //error
+  }
+}
+
+//scrolling
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+getPhotos();
