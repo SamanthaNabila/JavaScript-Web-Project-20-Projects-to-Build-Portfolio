2a06746875ec4caa4523b2ae054616290d3f512a
Jannatul Ferdous Nabila <samanthanabila47@gmail.com>
2026-05-15 12:30:14 +0600

    Dark mode light mode clean code

--- Light mood Dark mood/script.js
+++ Light mood Dark mood/script.js
@@ -6,26 +6,46 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
-//dark mode style
+//Dark light image mood
+function imageMode(color) {
+  image1.src = `image/undraw_conceptual_idea_${color}.svg`;
+  image2.src = `image/undraw_feeling_proud_${color}.svg`;
+  image3.src = `image/undraw_proud_coder_${color}.svg`;
+}
 
-function darkMode() {
-  nav.style.backgroundColor = "rgba(0 0 0 /50%)";
-  textBox.style.backgroundColor = "rgba(255 255 255 /50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.remove("fa-sun");
-  toggleIcon.children[1].classList.add("fa-moon");
-  image1.src = "image/undraw_conceptual_idea_dark.svg";
-  image2.src = "image/undraw_feeling_proud_dark.svg";
-  image3.src = "image/undraw_proud_coder_dark.svg";
+//toggle Dark light mood
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark ? "rgba(0 0 0 /50%)" : "rgba(0 0 0 /50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgba(0 0 0 /50%)"
+    : "rgba(0 0 0 /50%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? imageMode(dark) : imageMode(light);
 }
 
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
-    darkMode();
+    localStorage.setItem("theme", "light");
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    lightMode();
+    localStorage.setItem("theme", "dark");
+    toggleDarkLightMode(false);
   }
 }
 toggleSwitch.addEventListener("change", switchTheme);
+
+//check local storage for theam
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", "currentTheme");
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
